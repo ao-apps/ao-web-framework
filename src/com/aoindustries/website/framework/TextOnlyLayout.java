c0512b350d0f0c6c31c5c4fc6b23284da1b74a75
Dan Armstrong <dan@aoindustries.com>
2013-09-22 22:54:00 +0000

    Removed unused imports.

--- src/com/aoindustries/website/framework/TextOnlyLayout.java
+++ src/com/aoindustries/website/framework/TextOnlyLayout.java
@@ -1,10 +1,10 @@
-package com.aoindustries.website.framework;
-
 /*
- * Copyright 2003-2009 by AO Industries, Inc.,
+ * Copyright 2003-2013 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
+package com.aoindustries.website.framework;
+
 import com.aoindustries.io.ChainWriter;
 import java.io.IOException;
 import java.sql.SQLException;
